ce1ddcc06b091fee478f6528f629aa6c4785f84e
sergio Perez <sergio@sergio-HP-G62-Notebook-PC>
2012-08-02 19:14:01 +0200

    Inicio Proyecto

---
+++ modules/Index/IndexController.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Description of IndexController
+ *
+ * @since 03-08-2012
+ * @author Sergio Perez <sergio.perez@albatronic.com>
+ */
+class IndexController {
+    
+    public function IndexAction() {
+        
+    }
+}
+
+?>
