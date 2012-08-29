4657ae879af9de165e2cffab1ed99fb5eaa6bbbd
Administrador <Administrador@P702.ayuntamiento.es>
2012-08-29 17:00:58 +0200

    Actualizo CSS y JS

--- xxx 2012 08 27 js - copia/detecto_navegadores_viejos.php
+++
@@ -1,8 +0,0 @@
-<?php
-$navegador_php=$_SERVER["HTTP_USER_AGENT"];
-$decode_navegador_php = strpos($navegador_php, "MSIE 7");
- if($decode_navegador_php !== FALSE){$navegador_viejo="SI";}
-$decode_navegador_php = strpos($navegador_php, "MSIE 6");
- if($decode_navegador_php !== FALSE){$navegador_viejo="SI";}
-
-?>
